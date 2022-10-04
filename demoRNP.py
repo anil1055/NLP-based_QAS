669672a9dfcafde095e0ae3705a5c15ccef10568
Anil Guven <anilguven1055@gmail.com>
2022-10-04 22:50:31 +0300

    Add methods and preprocess python file

---
+++ demoRNP.py
@@ -0,0 +1,196 @@
+from collections import OrderedDict
+import nltk
+import string
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+import preProcess
+
+nlp = en_core_web_sm.load()
+def findAnswerType(pos_txt):
+	ansType = ''
+	if pos_txt == 'people' or pos_txt == 'person':
+		ansType = 'PERSON'
+	if pos_txt == 'nationality' or pos_txt == 'religion' or pos_txt == 'politic':
+		ansType = 'NORP'
+	if pos_txt == 'country' or pos_txt == 'city' or pos_txt == 'state':
+		ansType = 'GPE'
+	if pos_txt == 'company' or pos_txt == 'agency' or pos_txt == 'institution':
+		ansType = 'ORG'
+	if pos_txt == 'building' or pos_txt == 'airport' or pos_txt == 'highway' or pos_txt == ' bridge':
+		ansType = 'FAC'
+	if pos_txt == 'location' or pos_txt == 'range':
+		ansType = 'LOC'
+	if pos_txt == 'hurricane' or pos_txt == 'battle' or pos_txt == 'war':
+		ansType = 'EVENT'
+	if pos_txt == 'song' or pos_txt == 'title':
+		ansType = 'WORK_OF_ART'
+	if pos_txt == 'language':
+		ansType = 'LANGUAGE'
+	if pos_txt == 'time' or pos_txt == 'date' or pos_txt == 'period' or pos_txt == 'duration' or pos_txt == 'year' or pos_txt == 'month' or pos_txt == 'day':
+		ansType = 'DATE'
+	if pos_txt == 'percentage':
+		ansType = 'PERCENT'
+	if pos_txt == 'measurement' or pos_txt == 'weight' or pos_txt == 'distance':
+		ansType = 'QUANTITY'
+		
+	return ansType
+
+def demo(related_sentence, answerTerm, questionSearch, quesNormal, totalFindAnswer, methodNo, isLemmatization):
+    processes = ''
+    sentence = related_sentence
+    try:         
+        question = questionSearch.split(',')
+        if isLemmatization == True:          
+            sentence = preProcess.lemmaProcess(sentence)
+            quesNormal = preProcess.lemmaProcess(quesNormal)
+            answerTerm = preProcess.lemmaProcess(answerTerm)
+        
+        sentenceTerm = []
+        if methodNo == 1:            
+            question = preProcess.punctionProcess(quesNormal)  
+            question = question.lower().strip().split(' ')
+            sentence = preProcess.punctionProcess(sentence)  
+            sentence = sentence.lower().strip().split(' ')
+            answer = preProcess.punctionProcess(answerTerm)   
+            answer = answer.lower().strip().split(' ')
+            sw = stopwords.words('english')
+            processes += '\nRemoving question words in sentence: ' 
+            for ques in question:                
+                try: 
+                    sentence.remove(ques)
+                    processes += ques + ','
+                except: 
+                    for synset in wordnet.synsets(ques):
+                        cnt = False
+                        for syn in synset.lemmas():
+                            if str(txt).find(syn.name()) != -1:
+                                try:
+                                    sentence.remove(str(syn.name()))
+                                    cnt = True
+                                    break
+                                except: continue
+                        if cnt == True:
+                            break
+                    continue
+            processes += '\n\nRemoving stopwords in sentence: '
+            for stop in sw:
+                try: 
+                    sentence.remove(stop)
+                    processes += stop + ','                    
+                except: 
+                    continue
+            processes += '\n\nRemoving stopwords in answer: '
+            for stop in sw:
+                try: 
+                    answer.remove(stop)
+                    processes += stop + ',' 
+                except: 
+                    continue
+            if len(sentence) == len(answer):
+                sent = ''
+                ans = ''
+                for s in sentence:
+                    sent += s + ' '
+                for a in answer:
+                    ans += a + ' '
+                if sent.strip() == ans.strip():
+                    totalFindAnswer += 1
+
+        if methodNo == 2:
+            text = str(quesNormal).lower()
+            ansType = ''
+            if text.find('who') != -1:
+                ansType = 'PERSON'
+            if text.find('when') != -1:
+                ansType = 'DATE'
+            if text.find('how many') != -1:
+                ansType = 'QUANTITY'
+            if text.find('how much') != -1:
+                ansType = 'QUANTITY, MONEY'
+            if text.find('where') != -1:
+                ansType = 'GPE'
+            pos = preProcess.posTag(quesNormal.lower())
+            if len(pos)> 2:
+                for i in range(len(pos)-1):
+                    if pos[i][0] == 'what' or pos[i][0] == 'which' or pos[i][0] == 'where' or pos[i][0] == 'whose':
+                        ansType = findAnswerType(pos[i+1][0])
+                if ansType == '':
+                    for i in range(len(pos)-1):
+                        ansType = findAnswerType(pos[i+1][0])
+                if ansType != '':                   
+                    sentence = nlp(sentence)
+                    labels = [x.label_ for x in sentence.ents]
+                    sentences = [x for x in sentence.sents]
+                    processes += 'Question\'s NER label: ' + ansType + '\n\n'
+                    processes += 'Related sentence\'s NER labels:\n' 
+                    answer = ''
+                    ind = 0
+                    for k in nlp(str(sentences[0])).ents:
+                        processes += str(k) + ': ' + str(labels[ind]) + '\n' 
+                        ind += 1
+                    ind = 0
+                    for k in nlp(str(sentences[0])).ents:
+                        if ansType.find(labels[ind]) != -1 and text.find(str(k)) == -1:
+                            answer = str(k)
+                            break
+                        ind += 1
+                    if answer == answerTerm or answer == answerTerm.lower():
+                        totalFindAnswer += 1
+
+        if methodNo == 3:
+            pos = preProcess.posTag(quesNormal.lower())
+            ansType = ''
+            if len(pos) > 2:
+                for i in range(len(pos)-1):         
+                    if pos[i][1] == 'NNP' or pos[i][1] == 'WP$':
+                        if pos[i][0] == 'whose': ansType = "NNP"
+                    if pos[i][1] == 'WP' or pos[i][1] == 'WP':
+                        if pos[i][0] == 'who': ansType = "NNP"
+                        if pos[i][0] == 'what': ansType = "NN"
+                    if pos[i][1] == "WRB":
+                        if pos[i][0] == 'when': ansType = "CD"
+                        if pos[i][0] == 'how': 
+                            if pos[i+1][1] == 'JJ': ansType = "CD"
+                            else: ansType = "JJ,RB"
+                        if pos[i][0] == 'where': ansType = "NN"
+                        if pos[i][0] == 'why': ansType = "IN"
+                    if pos[i][1] == "WDT":
+                        ansType = "NN"
+                    if ansType != '':
+                        break
+        
+                if ansType != '':
+                    processes += 'Question\'s POS tag: ' + ansType + '\n\n'
+                    processes += 'Removing question terms in sentence: '
+                    question = quesNormal[:-1].split(' ')
+                    for ques in question:
+                        if sentence.find(ques) != -1:
+                            ind = sentence.index(ques)
+                            processes += ques + ','
+                            sentence = sentence[:ind-1] + sentence[ind+len(ques):]                 
+                    posSent = preProcess.posTag(sentence.strip())
+                    processes += '\nRelated sentence\'s POS tags:\n'
+                    ans = ''
+                    ind = 0
+                    for key, p in posSent:
+                        processes += str(key) + ': ' + str(p) + '\n' 
+                        ind += 1
+                    ind = 0
+                    for key, p in posSent:
+                        if ansType.find(p) != -1:
+                            ans += str(key) + ' '
+                            if ind+1 < len(posSent)-1:
+                                for k in range(ind+1,len(posSent)):
+                                    if ansType.find(str(posSent[k][1])) != -1:
+                                        ans += str(posSent[k][0]) + ' '
+                                    else: break
+                            break
+                        ind += 1
+                    if ans != '':
+                        if ans[:-1] == answerTerm or ans[:-1] == answerTerm.lower():
+                            totalFindAnswer += 1        
+
+    except Exception as e:
+        print('Error Answer detection: ' + str(e) + ' - method:' + str(methodNo))        
+    return totalFindAnswer, processes
---
+++ preProcess.py
@@ -0,0 +1,169 @@
+def punctionProcess(text):
+    out = str(text).maketrans('','', string.punctuation)
+    result = str(text).translate(out)
+    
+    return result
+
+#punktTokenizer for sentence detection
+def punktSentenceTokenizer(paragraph):
+    sent_tokenizer = nltk.tokenize.PunktSentenceTokenizer()
+    sentences = sent_tokenizer.tokenize(paragraph)
+    updateSentence = solveSentenceTokenizer(sentences)
+    if len(updateSentence) == 0:
+        updateSentence = sentences
+
+    return updateSentence
+
+#POS Tagging
+def posTag(text):
+    txt = word_tokenize(text)
+    pos = nltk.pos_tag(txt)
+
+    return pos
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+# Lemmatize with POS Tag
+from nltk.corpus import wordnet
+def get_wordnet_pos(word):
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
+
+lemmatizer=WordNetLemmatizer()
+def lemmaProcess(sentence):       
+    input_str=word_tokenize(sentence)
+    txt = ''
+    for word in input_str:
+        txt += lemmatizer.lemmatize(word, get_wordnet_pos(word)) + ' ' #, get_wordnet_pos(word)
+    sentence = str(txt).strip()
+
+    return sentence
+
+import spacy
+from spacy import displacy
+from collections import Counter
+import en_core_web_sm
+
+nlp = en_core_web_sm.load()
+#NER process for preprocessing
+def NerRecognition(article, answ_list):
+    article = nlp(article)
+    
+    #labels = [x.label_ for x in article.ents]
+    #Counter(labels)
+    #sentences = [x for x in article.sents]
+    #print(dict([(str(x), x.label_) for x in nlp(str(sentences[0])).ents]))
+
+    #Removed number delete process: number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] ner_list = [] delete = [] for ner in ner_: ner_list.append(ner) for sayi in number: if str(ner).find(sayi) != -1: delete.append(ner) break for sil in delete: ner_list.remove(sil)
+    items = [x.text for x in article.ents]   
+    ner_list = Counter(items).keys()
+    sw = stopwords.words('english') 
+    for ner in ner_list:
+        if str(ner).find('\'s') != -1:
+            indx = str(ner).index('\'s')
+            ner = ner[:indx]
+        if str(ner).find(' ') != -1:                  
+            ner_ = str(ner).split(' ')
+            islow = False
+            if str(ner_[len(ner_)-1]).islower() == True:
+                islow = True
+            try:
+                ind = sw.index(ner_[0])
+            except:
+                uri = ""
+                for i in ner_:
+                    if islow == False:
+                        try:
+                            try:
+                                ner_list.index(i)
+                            except:
+                                ind = answ_list.index(i)
+                                answ_list.remove(str(i).strip())
+                            uri += str(i)  + "_" 
+                        except:
+                            uri += str(i) + "_"                            
+                if uri != "": answ_list.append(str(uri[:-1]))
+        else:
+            for ans in answ_list:
+                if ans.find(ner) != -1:
+                    answ_list.remove(ans)
+                    answ_list.append(ner)
+                    break
+    return answ_list
+
+
+#developed solve for sentence tokenizer error
+def solveSentenceTokenizer(sentences):
+    firstStage = []
+    ind = 0
+    indList = []
+    last = []
+    #If next sentence first word's first letter is lower or numeric char, combine with sentences
+    s = ''
+    for sentence in sentences:
+        if ind+1 < len(sentences):
+            words = word_tokenize(sentences[ind+1])
+            word = words[0]
+            if str(word).islower() == True or str(word).isnumeric() == True:
+                firstStage.append(sentence+ ' ' + sentences[ind+1])
+                s += sentence+ ' ' + sentences[ind+1]
+                last.append(ind+1)
+                if ind+1 - last[len(last)-2] == 1:
+                    sent = firstStage[len(firstStage)-2]
+                    firstStage.remove(firstStage[len(firstStage)-2])
+                    firstStage.remove(firstStage[len(firstStage)-1])
+                    sent += ' ' + sentences[ind+1]
+                    firstStage.append(sent)
+                    s += sent
+            else:
+                firstStage.append(sentence)  
+        else:
+                firstStage.append(sentence) 
+        ind += 1
+    for ind in range(0, len(firstStage)-1):
+        if str(firstStage[ind]).find(firstStage[ind+1]) != -1:
+            indList.append(ind+1)
+    indList.sort(reverse=True)
+    for ind in indList:
+        firstStage.remove(firstStage[ind])
+    #If sentence containing max 2 word, combines with next sentences
+    secondStage = []
+    ind = 0
+    for sentence in firstStage:
+        words = word_tokenize(sentence) 
+        if len(words) < 3:
+            if  ind+1 < len(firstStage):
+                secondStage.append(sentence + ' ' + firstStage[ind+1])
+        else:
+            if len(secondStage) > 0:
+                if str(secondStage[len(secondStage)-1]).find(sentence) == -1:
+                    secondStage.append(sentence)
+            else:
+                secondStage.append(sentence)
+        ind += 1
+    #Previous word is uppercase, but last word's first character is uppercase with max 3 length => combine with next sentences 
+    lastSentences = []
+    ind = 0
+    for update in secondStage:
+        words = word_tokenize(update) 
+        if len(words) >= 2:
+            word = words[len(words)-2]
+            if len(lastSentences) != 0:
+                if str(lastSentences[len(lastSentences)-1]).find(update) != -1:
+                    ind += 1
+                    continue
+            if str(word[0]).isupper() == True and len(word) < 4 and len(words) >= 3:
+                word = words[len(words)-3]
+                if str(word[0]).islower() == True and len(secondStage) > ind+1:
+                    lastSentences.append(update + ' ' + secondStage[ind+1])
+                else:
+                    lastSentences.append(update)
+            else:
+                lastSentences.append(update)
+        ind += 1
+    return lastSentences
